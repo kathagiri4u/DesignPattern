8ffb97159af6e1c6392234e3fd85957a67cacc26
kathagiri4u <kathagiri4u@gmail.com>
2022-03-07 20:31:15 -0700

    StringOperations

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
+using DesignPattern.Strings;
 
 namespace DesignPattern
 {
@@ -9,35 +8,8 @@ namespace DesignPattern
             static void Main(string[] args)
             {
                 Console.WriteLine("Hello World!");
-                RepeatedString();
+                StringOps.RepeatedString();
             }
-
-        public static void RepeatedString()
-        {
-            string[] a = new string[]{"cat", "dog", "cat", "cat", "horse", "duck", "Duck", };
-            string[]b = a;
-            int count = 0;
-            int i=0;
-
-            for(i = 0; i <a.Length; i++)
-            {
-                for ( int j =0; j<a.Length; j++)
-                {
-                    if(a[i].ToLower()==a[j].ToLower())
-                    {
-                        count++;
-                        
-                    }
-
-                }
-                Console.WriteLine( "{0} Repeated {1} Times", a[i], count);
-                a=a.Where(w => w.ToLower() != a[i].ToLower()).ToArray();
-                --i;
-                count=0;
-
-            }
-
-        }
 
     }
 }
